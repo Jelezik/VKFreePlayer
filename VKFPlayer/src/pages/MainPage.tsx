5c7d405e85cfe7df53d9721080aa2f452cf1c1a9
vlad <nekita.avdoshin@gmail.com>
2023-02-07 14:33:40 +0300

    del useeffect

--- VKFPlayer/src/pages/MainPage.tsx
+++ VKFPlayer/src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Button, Text, View} from 'react-native';
 import VKLogin from 'react-native-vkontakte-login';
 import {useAppDispatch} from '../redux/utils/redux-utils';
